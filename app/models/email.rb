c6d2b7d05fbb9a0be3db55a0169bf14bc46f2744
Vasco Conde <vc@vascoconde.com>
2015-09-17 19:36:31 +0100

    Add HTTParty gem, create email validator class that calls API to validate email addresses

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,6 +2,8 @@ class Email < ActiveRecord::Base
   after_create :schedule_email
 
   def schedule_email
+  	response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+
   	ScheduledEmailJob.set(wait_until: self.send_date).perform_later(self)
   end
 end
