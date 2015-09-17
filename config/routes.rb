28e129ade77167b179ffb817614eba6a033394c6
Vasco Conde <vc@vascoconde.com>
2015-09-17 20:08:38 +0100

    Remove edit and show actions from email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :emails
+  resources :emails, except: [:show, :edit, :update]
   root 'emails#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
