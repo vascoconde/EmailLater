0bbd72143726afcdb779136f05b83b8eb1f47b78
Vasco Conde <vc@vascoconde.com>
2015-09-17 20:19:46 +0100

    EmailValidaor parse JSON and returns boolean

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -2,12 +2,14 @@ require "httparty"
 
 module EmailValidator
   class EmailValidator
-  	def validate(email)
-      HTTParty.get "https://pozzad-email-validator.p.mashape.com/emailvalidator/validateEmail/#{email}",
+  	def self.validate(email)
+
+      response = HTTParty.get "https://pozzad-email-validator.p.mashape.com/emailvalidator/validateEmail/#{email}",
   		headers:{
   		  "X-Mashape-Key" => "Rl6gf5Oh1emshO5dmeoszGCdy4vPp1D7miJjsnU6go2Lj0WjNA",
   		  "Accept" => "application/json"
   		}
+  	  JSON.parse(response.body)['isValid']
     end
   end
 end
