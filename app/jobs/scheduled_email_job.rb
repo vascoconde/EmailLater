fd4d30a291002a7c27c8a42e8d971967a52e053b
Vasco Conde <vc@vascoconde.com>
2015-09-17 01:57:52 +0100

    Configure Sendgrid

--- app/jobs/scheduled_email_job.rb
+++ app/jobs/scheduled_email_job.rb
@@ -2,6 +2,7 @@ class ScheduledEmailJob < ActiveJob::Base
   queue_as :default
 
   def perform(email)
+  	ScheduledEmailMailer.scheduled_email(email).deliver_now
   	email.sent = true
   	email.save!
   end
