20f94bcdc63ca3574de3d0b3a3d75563b126da53
Vasco Conde <vc@vascoconde.com>
2015-09-18 18:22:40 +0100

    Raise exception in BackgroundJob if email is not verified

--- app/jobs/scheduled_email_job.rb
+++ app/jobs/scheduled_email_job.rb
@@ -2,6 +2,8 @@ class ScheduledEmailJob < ActiveJob::Base
   queue_as :default
 
   def perform(email)
+  	raise "Email not verified" unless email.verified
   	ScheduledEmailMailer.scheduled_email(email).deliver_now
+  	email.destroy!
   end
 end
